d32482a0c599fdbb3ea1b12b52b0be99c207ab8c
sining06 <46968513+sining06@users.noreply.github.com>
2019-02-08 16:56:26 +0100

    Delete script.js

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-function affiche() {
-    var bouton = this.id;
-    $(".bouton").removeClass("Red");
-    $(".texte").hide();
-    $("#" + bouton).addClass("Red");
-    $("#" + bouton + "_text").show();
-}
-$(".bouton").on("click", affiche);
